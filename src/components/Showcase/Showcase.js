91e12a861258fb2e0d38c94e9c7cc21606f05e3f
Taylor Clay <8163665+TaylorClay@users.noreply.github.com>
2021-02-12 16:36:39 -0600

    Improve Showcase Description Display (#4)

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -79,7 +79,7 @@ function ShowCase({ title, slug, myList, myListAddHandler, myListRemoveHandler }
               {movieTitle}
             </h1>
             <p title={overview}>
-              {_trimWordLength(overview, document.body.clientWidth / 20)}
+              {_trimWordLength(overview, document.body.clientWidth / 25)}
             </p>
             <div id="Showcase-button-group">
               <button
