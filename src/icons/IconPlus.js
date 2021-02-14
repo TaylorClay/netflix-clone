1e9181ad26588a993d22f4d06d2853155f6889e9
Taylor Clay <Taylor.Clay@cerner.com>
2021-02-14 09:23:08 -0600

    Added search button

--- src/icons/IconPlus.js
+++ src/icons/IconPlus.js
@@ -1,6 +1,6 @@
 function IconPlus() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <svg role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <title>Addition icon</title>
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
     </svg>
