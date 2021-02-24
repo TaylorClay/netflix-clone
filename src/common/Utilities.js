2f858c6ccae2a31c93b5dc518b705d680e6c3d2e
Taylor Clay <Taylor.Clay@cerner.com>
2021-02-24 09:38:48 -0600

    Clean up jsdoc

--- src/common/Utilities.js
+++ src/common/Utilities.js
@@ -2,9 +2,9 @@
  * Trims a String to a given number of characters, and then appends an ellipsis.
  * If the String is less than the max number of characters, the original String is returned.
  * 
- * @param textToTrim - The String to be trimmed (if necessary)
- * @param maxLength - The maximum allowed characters before being trimmed
- * @returns {string} - The trimmed String
+ * @param textToTrim The String to be trimmed (if necessary)
+ * @param maxLength The maximum allowed characters before being trimmed
+ * @returns {string} The trimmed String
  */
 export function _trimTextLength(textToTrim, maxLength = 50) {
   if (!textToTrim) return textToTrim;
@@ -15,9 +15,9 @@ export function _trimTextLength(textToTrim, maxLength = 50) {
  * Trims a String to a given number of words, and then appends an ellipsis.
  * If the String is less than the max number of words, the original String is returned.
  * 
- * @param textToTrim - The String to be trimmed (if necessary)
- * @param maxWordCount - The maximum allowed words before being trimmed
- * @returns {string} - The trimmed String
+ * @param textToTrim The String to be trimmed (if necessary)
+ * @param maxWordCount The maximum allowed words before being trimmed
+ * @returns {string} The trimmed String
  */
 export function _trimWordLength(textToTrim, maxWordCount = 100) {
   if (!textToTrim) return textToTrim;
@@ -27,11 +27,11 @@ export function _trimWordLength(textToTrim, maxWordCount = 100) {
 }
 
 /**
- * Determines if a list contains a target Id.
+ * Determines if a list contains a target Id
  * 
- * @param list - The list to search
- * @param id - The id to search for
- * @returns {boolean} - Whether or not the Id exists in the list
+ * @param list The list to search
+ * @param id The id to search for
+ * @returns {boolean} Whether or not the Id exists in the list
  */
 export function _getIsIdInList(list, id) {
   return list.findIndex(listItem => listItem.id === id) >= 0;
